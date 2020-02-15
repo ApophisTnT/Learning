19eceb78bab1ae9f9df32f20a1b53aa2443e73d0
ApophisTnT <34527214+ApophisTnT@users.noreply.github.com>
2020-02-15 18:22:43 +0800

    Create maxSubArray.cpp

---
+++ Leetcode/maxSubArray.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Deserve on 2020/2/14.
+//
+#include <stdio.h>
+#include<malloc.h>
+#define M 9
+
+//int maxSubArray(int *nums,int numSize){
+//    int max=*nums,temp=0;
+//    for (int i = 0; i < numSize; ++i) {
+//        if(temp<0) temp=nums[i];
+//        else temp+=nums[i];
+//        if(temp>max) max=temp;
+//    }
+//    return max;
+//}
+//
+//int main(){
+//    int nums[]={-212121312},re=-1;
+//    re=maxSubArray(nums,1);
+//    printf("%d",re);
+//    return 0;
+//
+//
+//}
+
